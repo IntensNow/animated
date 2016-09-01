edff416e4c3238acb640f3853d32cb6816f76e31
IntensNow <d.korobskoy@mail.ru>
2016-09-01 22:38:36 +0300

    Add files via upload

--- js/preloader.js
+++ js/preloader.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
       $('#preloader').addClass('preloader');
        setTimeout(function(){
            $('#preloader').css('display','none');
+           $('.main-title').css({animation:'fadeIn 3s ease-in-out',
+                                opacity:'1'});
+           $('.see_more').css({animation:'slideInUp 2s ease-in-out',
+                                visibility:'visible'});
        },3000); 
   },1000); 
     /*Fullpage initialization*/
     $('#fullpage').fullpage();
     /*move buttons methods*/
 //adding the action to the button
-    $(document).on('click', '.about_me', function(){
+    $(document).on('click', '.see_more', function(){
   $.fn.fullpage.moveSectionDown();});
     $(document).on('click', '.scroll_down', function(){
   $.fn.fullpage.moveSectionDown();});
